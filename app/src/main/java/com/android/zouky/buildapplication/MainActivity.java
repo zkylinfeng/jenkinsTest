898c6b1a1da689091fa87c59755585b4491792ac
zkylinfeng <2425759217@qq.com>
2018-02-04 20:18:59 +0800

    zky

--- app/src/main/java/com/android/zouky/buildapplication/MainActivity.java
+++ app/src/main/java/com/android/zouky/buildapplication/MainActivity.java
@@ -12,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TextView textView = findViewById(R.id.tv);
         textView.setText("hello world!");
+        textView.setText("hello world!");
+        textView.setText("hello world!");
     }
 }
